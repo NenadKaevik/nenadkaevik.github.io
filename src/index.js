ac6c302b01ea170ed23708f7e46142d5db4562e8
Nenad Kaevik <nenadkaevik@gmail.com>
2019-01-25 13:20:52 +0100

    Init commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'jquery/src/jquery';
+import 'popper.js';
+import 'bootstrap/dist/css/bootstrap-grid.css';
+import 'bootstrap/dist/css/bootstrap-reboot.css';
+import '@fortawesome/fontawesome-free/js/all';
+import './sass/styles.scss';
+
+let gravatar = require('gravatar');
+let url = gravatar.url('nenadkaevik@gmail.com', {protocol: 'https', s: '100'});
+
+$(document).ready(function(){
+    $(".avatar img").attr("src",url);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: 'bundle.js'
-  }
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$|\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [
+                        "css-loader", // translates CSS into CommonJS
+                        "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                    ]
+                })
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                'file-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("styles.css"),
+    ]
 };
